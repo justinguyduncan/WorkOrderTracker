c5faf1a703eadc7c07875615f5129c08a19c7be6
Justin Duncan <justinguyduncan@hotmail.com>
2023-09-05 19:07:19 -0700

    added to index

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,15 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import departmentReducer from './department';
+import jobReducer from './job';
+import noteReducer from './note';
 
 const rootReducer = combineReducers({
   session,
+  departmentReducer,
+  jobReducer,
+  noteReducer
 });
 
 
